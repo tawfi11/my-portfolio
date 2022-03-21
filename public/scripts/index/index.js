c5f4db32f9160b318530c9d5ae429f45a5891561
tawfi11 <nusair11@gmail.com>
2022-03-20 17:14:00 -0700

    started uniform website

--- public/scripts/index/index.js
+++ public/scripts/index/index.js
@@ -2,31 +2,30 @@ let checkScroll = true;
 let state = 1;
 let oldState = 0;
 let returnState = 1;
+let canMove = true;
 $(".page").css("display", "none");
 $(`#state${state}`).css("display", "inline");
 
 $(window).bind('mousewheel DOMMouseScroll', function(event){
-    if(checkScroll){
-        console.log(state);
+    if(checkScroll && canMove){
+        //console.log(state);
         let scrollDir = 0;
         if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-            console.log('scroll up');
+            //console.log('scroll up');
             scrollDir = 1;
         }
         else {
-            console.log('scroll down');
+            //console.log('scroll down');
             scrollDir = -1;
         }
         checkScroll = false;
 
         if(state == 1 || state == 0){
-            state = state1(scrollDir, oldState);
+            state = state1(scrollDir);
         }
-
-        if(returnState != state){
-            oldState = state;
+        else if(state == 2){
+            state = state2(scrollDir);
         }
-        returnState = state;
     }
 });
 
--- public/scripts/index/states.js
+++ public/scripts/index/states.js
@@ -1,49 +1,126 @@
-//ScrollDir = -1 --> scrolling UP
-//ScrollDir = 1 --> scrolling DOWN
+//ScrollDir = -1 --> scrolling DOWN
+//ScrollDir = 1 --> scrolling UP
 //ScrollDir = 0 --> no scroll
+const opacitySpeed = 0.05;
+const scrollSpeed = 5;
+const opacityLimit = 0.75;
+
+function colorCorrection(location){
+    $(".nav-link").css("color", "grey");
+    $(`#${location}Btn`).css("color", "yellow");
+    return;
+}
+
+function stateTransition(newState, oldState, location){
+    $(`#${oldState}`).css("display", "none");
+    $(`#${newState}`).css("display", "initial");
+    $(`#${newState}`).css("opacity", 0);
+    $(`#${newState}`).css("top", "0px");
+    canMove = false;
+    colorCorrection(location);
+    $(`#${newState}`).fadeTo("slow", opacityLimit, function(){
+        canMove = true;
+    });
+    return;
+}
 
 //state 1 solar system --> ABOUT ME page (state 3)
-function state1(scrollDir, oldState){
-    const scrollSpeed = 5;
-    const opacitySpeed = 0.05;
+function state1(scrollDir){
+    colorCorrection("Home");
     let opacity = parseFloat($(`#state1`).css("opacity"));
     let y = parseFloat($(`#state1`).css("top"));
     returnState = 1;
     if(scrollDir == -1){
-        if(oldState == 0){
-            if(opacity - opacitySpeed >= 0){
-                opacity -= opacitySpeed;
-                y -= scrollSpeed;
-                console.log(opacity);
-            }
-            else {
-                opacity = 0;
-                y = 0;
-                $("#state1").css("display", "none");
-                $("#state3").css("display", "inline");
-                let state3Y = 1 / opacitySpeed * scrollSpeed;
-                $("#state3").css("top", `${state3Y}px`);
-                returnState = 3;
-            }
+        if(opacity - opacitySpeed >= 0){
+            opacity -= opacitySpeed;
+            y -= scrollSpeed;
+            //console.log(opacity);
+        }
+        else {
+            opacity = 0;
+            y = 0;
+            /*$("#state1").css("display", "none");
+            $("#state2").css("display", "initial");
+            $("#state2").css("opacity", 0);
+            $("#state2").css("top", "0px");
+            canMove = false;
+            $(".nav-link").css("color", "grey");
+            $("#AboutBtn").css("color", "yellow");
+            $("#state2").fadeTo("slow", opacityLimit, function(){
+                canMove = true;
+            });*/
+            stateTransition("state2", "state1", "About");
+            returnState = 2;
+            //$("#state2").css("display", "inline");
+            //let state3Y = 1 / opacitySpeed * scrollSpeed;
+            //$("#state2").css("top", `${state3Y}px`);
+            
         }
     }
     else if(scrollDir == 1){
-        if(oldState == 0){
-            if(opacity + opacitySpeed <= 0.75){
-                opacity += opacitySpeed;
-                y += scrollSpeed;
-            }
+        if(opacity + opacitySpeed <= opacityLimit){
+            opacity += opacitySpeed;
+            y += scrollSpeed;
         }
+        
     }
     $("#state1").css("opacity", opacity);
     $("#state1").css("top", `${y}px`);
     return returnState;
 }
 
-//ABOUT ME => Who I am (state 3) --> ABOUT ME => Exerpt (state 5)
-//ABOUT ME => Who I am (state 3) --> solar system (state 2) 
+//ABOUT ME => Who I am (state 2) --> ABOUT ME => Exerpt (state 3)
+//ABOUT ME => Who I am (state 2) --> solar system (state 1) 
 function state2(scrollDir){
-    const scrollSpeed = 5;
-    const opacitySpeed = 0.05;
-    
+    colorCorrection("About");
+    let opacity = parseFloat($(`#state2`).css("opacity"));
+    let y = parseFloat($(`#state2`).css("top"));
+    returnState = 2;
+    if(scrollDir == -1){
+        y-= scrollSpeed;
+        if(y <= 0){
+            opacity -= opacitySpeed;
+            if(opacity <= 0){ // Transition into solar system
+                opacity = 0;
+                y = 0;
+                /*$("#state2").css("display", "none");
+                $("#state3").css("top", "0px");
+                canMove = false;
+                $("#state3").fadeTo("slow", opacityLimit, function(){
+                    canMove = true;
+                });*/
+                stateTransition("state3", "state2", "Education");
+                returnState = 3;
+            }
+        }
+        else {
+            opacity += opacitySpeed;
+        }
+    }
+    else if(scrollDir == 1){
+        y+= scrollSpeed;
+        if(y >= 0){
+            opacity -= opacitySpeed;
+            if(opacity <= 0){ // Transition into solar system
+                opacity = 0;
+                y = 0;
+                /*$("#state2").css("display", "none");
+                $("#state1").css("top", "0px");
+                canMove = false;
+                $(".nav-link").css("color", "grey");
+                $("#HomeBtn").css("color", "yellow");
+                $("#state1").fadeTo("slow", opacityLimit, function(){
+                    canMove = true;
+                });*/
+                stateTransition("state1", "state2", "Home");
+                returnState = 1;
+            }
+        }
+        else {
+            opacity += opacitySpeed;
+        }
+    }
+    $("#state2").css("opacity", opacity);
+    $("#state2").css("top", `${y}px`);
+    return returnState;
 }
